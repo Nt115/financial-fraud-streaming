bb7e844e808b1f8c04b456c7f78f506aca4fa705
Nt115 <71083294+Nt115@users.noreply.github.com>
2026-06-20 13:59:57 +0530

    Create analytics_db and transactions_raw table

--- database/create_hive_tables.sql
+++ database/create_hive_tables.sql
@@ -1,1 +1,17 @@
+CREATE DATABASE IF NOT EXISTS analytics_db;
+USE analytics_db;
+
+CREATE TABLE IF NOT EXISTS transactions_raw (
+    transaction_id STRING,
+    account_id STRING,
+    tx_timestamp TIMESTAMP,
+    amount DOUBLE,
+    merchant STRING,
+    category STRING,
+    location_country STRING,
+    device_id STRING,
+    is_fraud_flag INT
+)
+PARTITIONED BY (tx_date STRING)
+STORED AS PARQUET; -- Parquet storage ensures high-speed compression and execution
 
